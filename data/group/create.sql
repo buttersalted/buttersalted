151ca963527373732dfb8ba68602533c8e19af27
Subhajit Sahu <wolfram77@gmail.com>
2017-09-15 17:32:42 +0530

    :tv: GroupData create table

--- data/group/create.sql
+++ data/group/create.sql
@@ -1,10 +1,30 @@
 CREATE TABLE IF NOT EXISTS "group" (
-  "field" TEXT NOT NULL,
-  "value" TEXT NOT NULL,
-  "query" TEXT NOT NULL,
-  PRIMARY KEY ("field", "value"),
+  "id"    TEXT NOT NULL,
+  "key"   TEXT NULL,
+  "tag"   TEXT NULL,
+  "value" TEXT NOT NULL DEFAULT 'SELECT NULL LIMIT 0',
+  PRIMARY KEY ("id"),
+  UNIQUE ("key", "tag"),
   CHECK (
-    "field" <> '' AND "value" <> '' AND
-    "query" NOT LIKE '%;%' AND lower("query") LIKE 'select %'
+    "id" <> '' AND
+    ("key" = NULL OR "key" <> '') AND
+    ("tag" = NULL OR "tag" <> '') AND
+    "value" NOT LIKE '%;%' AND
+    lower("value") LIKE 'select %' AND
+    lower("value") NOT LIKE '% into %'
   )
 );
+
+CREATE OR REPLACE FUNCTION "group_executeone" (
+  IN _field TEXT,
+  IN _value TEXT
+) AS $$
+DECLARE
+  _query TEXT;
+BEGIN
+  SELECT "query" INTO _query FROM "group"
+  WHERE "field"=_field AND "value"=_value;
+  EXECUTE 'WITH ('||_query||') AS t '||
+  'UPDATE t SET "'||_field||'"='
+END;
+$$ LANGUAGE plpgsql;
