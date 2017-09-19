a7583c7dc7f372bad79e767964d24d5ac0020ab3
Subhajit Sahu <wolfram77@gmail.com>
2017-09-19 12:41:04 +0530

    :tv: log req.params

--- sql/index.js
+++ sql/index.js
@@ -4,7 +4,7 @@ const express = require('express');
 module.exports = function(db) {
   const x = express();
   x.get('/:val', (req, res) => {
-    console.log(req.body);
+    console.log(req.params);
     db.query(req.body.val, []).then((ans) => res.send(ans.rows||[]));
   });
   return x;
