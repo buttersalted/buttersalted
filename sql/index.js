329eed1787a91eb37fc9161bf0c4d1a93eb72265
Subhajit Sahu <wolfram77@gmail.com>
2017-09-19 12:35:19 +0530

    :tv: log in sql

--- sql/index.js
+++ sql/index.js
@@ -3,6 +3,9 @@ const express = require('express');
 
 module.exports = function(db) {
   const x = express();
-  x.get('/:val', (req, res) => db.query(req.body.val).then((ans) => res.send(ans.rows||[])));
+  x.get('/:val', (req, res) => {
+    console.log(req.body.val);
+    db.query(req.body.val, []).then((ans) => res.send(ans.rows||[]));
+  });
   return x;
 };
