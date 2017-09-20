d4de89ea3ba108d08d6aa20660b09fc06922012d
Subhajit Sahu <wolfram77@gmail.com>
2017-09-20 13:59:51 +0530

    :tv: use res.json (its like mcclaren f1)

--- json/group.js
+++ json/group.js
@@ -3,12 +3,12 @@ const express = require('express');
 
 module.exports = function(src) {
   const x = express();
-  x.get('/', (req, res, next) => src.select(req.body).then((ans) => res.send(ans.rows||[]), next));
-  x.post('/', (req, res, next) => src.insertOne(req.body).then((ans) => res.send(1), next));
-  x.get('/:id', (req, res, next) => src.selectOne(req.body).then((ans) => res.send(ans.rows||[]), next));
-  x.patch('/:id', (req, res, next) => src.upsertOne(req.body).then((ans) => res.send(1), next));
-  x.delete('/:id', (req, res, next) => src.deleteOne(req.body).then((ans) => res.send(1), next));
-  x.get('/:id/execute', (req, res, next) => src.executeOne(req.body).then((ans) => res.send(1), next));
-  x.get('/:id/unexecute', (req, res, next) => src.unexecuteOne(req.body).then((ans) => res.send(1), next));
+  x.get('/', (req, res, next) => src.select(req.body).then((ans) => res.json(ans.rows||[]), next));
+  x.post('/', (req, res, next) => src.insertOne(req.body).then((ans) => res.json(1), next));
+  x.get('/:id', (req, res, next) => src.selectOne(req.body).then((ans) => res.json(ans.rows||[]), next));
+  x.patch('/:id', (req, res, next) => src.upsertOne(req.body).then((ans) => res.json(1), next));
+  x.delete('/:id', (req, res, next) => src.deleteOne(req.body).then((ans) => res.json(1), next));
+  x.get('/:id/execute', (req, res, next) => src.executeOne(req.body).then((ans) => res.json(1), next));
+  x.get('/:id/unexecute', (req, res, next) => src.unexecuteOne(req.body).then((ans) => res.json(1), next));
   return x;
 };
--- json/type.js
+++ json/type.js
@@ -3,10 +3,10 @@ const express = require('express');
 
 module.exports = function(src) {
   const x = express();
-  x.get('/', (req, res, next) => src.select(req.body).then((ans) => res.send(ans.rows||[]), next));
-  x.post('/', (req, res, next) => src.insertOne(req.body).then((ans) => {console.log('res'); res.send(1);}, next));
-  x.get('/:id', (req, res, next) => src.selectOne(req.body).then((ans) => res.send(ans.rows||[]), next));
-  x.patch('/:id', (req, res, next) => src.upsertOne(req.body).then((ans) => res.send(1), next));
-  x.delete('/:id', (req, res, next) => src.deleteOne(req.body).then((ans) => res.send(1), next));
+  x.get('/', (req, res, next) => src.select(req.body).then((ans) => res.json(ans.rows||[]), next));
+  x.post('/', (req, res, next) => src.insertOne(req.body).then((ans) => res.json(1), next));
+  x.get('/:id', (req, res, next) => src.selectOne(req.body).then((ans) => res.json(ans.rows||[]), next));
+  x.patch('/:id', (req, res, next) => src.upsertOne(req.body).then((ans) => res.json(1), next));
+  x.delete('/:id', (req, res, next) => src.deleteOne(req.body).then((ans) => res.json(1), next));
   return x;
 };
