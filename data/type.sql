d851c69a23a8f0cbbf158993e22fb1183cfb74ea
Subhajit Sahu <wolfram77@gmail.com>
2017-09-22 23:20:12 +0530

    :tv: update type (10-10)

--- data/type.sql
+++ data/type.sql
@@ -5,29 +5,27 @@ CREATE TABLE IF NOT EXISTS "type" (
   PRIMARY KEY ("id"),
   CHECK ("id"<>'' AND "value"<>'')
 );
--- 2. create index for value (faster search i hope)
 CREATE INDEX IF NOT EXISTS "type_value_idx"
 ON "type" ("value");
 
 
-CREATE OR REPLACE FUNCTION "type_insertone" (
-  IN _a JSON
-) RETURNS VOID AS $$
+CREATE OR REPLACE FUNCTION "type_insertone" (_a JSON)
+RETURNS VOID AS $$
 DECLARE
--- 1. get id, value, index from input (and set proper case)
+-- 1. get id, value, index
   _id    TEXT := _a->>'id';
   _value TEXT := _a->>'value';
   _index TEXT := coalesce(_a->>'index', 'btree');
 BEGIN
-  -- 2. add column id to food table with index (if its a column)
+  -- 2. insert into table (fail early)
+  INSERT INTO "type" VALUES (_id, _value);
+  -- 3. add column id to food table with index (if column)
   IF _value<>'TABLE' THEN
     EXECUTE format('ALTER TABLE "food" ADD COLUMN IF NOT EXISTS %I %s',
       _id, _value);
     EXECUTE format('CREATE INDEX IF NOT EXISTS %I ON "food" USING %s (%I)',
       'food_'||_id||'_idx', _index, _id);
   END IF;
-  -- 3. insert into table using id, value
-  INSERT INTO "type" VALUES (_id, _value);
 END;
 $$ LANGUAGE plpgsql;
 
