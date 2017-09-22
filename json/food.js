ad8d43181fe6a3649443884eda58c3506a14eeec
Subhajit Sahu <wolfram77@gmail.com>
2017-09-22 22:50:21 +0530

    :tv: unused properties

--- json/food.js
+++ json/food.js
@@ -9,9 +9,5 @@ module.exports = function(src) {
   x.get('/:id', (req, res, next) => src.selectOne(body(req)).then((ans) => res.json(ans.rows||[]), next));
   x.patch('/:id', (req, res, next) => src.upsertOne(body(req)).then((ans) => res.json(1), next));
   x.delete('/:id', (req, res, next) => src.deleteOne(body(req)).then((ans) => res.json(1), next));
-  x.get('/:id/execute', (req, res, next) => src.executeOne(body(req)).then((ans) => res.json(1), next));
-  x.get('/:id/unexecute', (req, res, next) => src.unexecuteOne(body(req)).then((ans) => res.json(1), next));
-  x.get('/pending', (req, res, next) => src.pendingInsert(body(req)).then((ans) => res.json(1), next));
-  x.delete('/pending/:id', (req, res, next) => src.pendingDeleteOne(body(req)).then((ans) => res.json(1), next));
   return x;
 };
