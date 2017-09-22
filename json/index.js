eea270df8a474bcc2698a24858299fbc6ec96d9c
Subhajit Sahu <wolfram77@gmail.com>
2017-09-22 22:53:47 +0530

    :tv: new JsonTable

--- json/index.js
+++ json/index.js
@@ -1,17 +1,13 @@
 'use strict';
 const express = require('express');
-const food = require('./food');
-const group = require('./group');
-const term = require('./term');
-const type = require('./type');
-const unit = require('./unit');
+const JsonTable = require('./jsontable');
 
 module.exports = function Json(src) {
   const x = express();
-  x.use('/food', food(src.food));
-  x.use('/group', group(src.group));
-  x.use('/term', term(src.term));
-  x.use('/type', type(src.type));
-  x.use('/unit', unit(src.unit));
+  x.use('/food', JsonTable(src.food));
+  x.use('/group', JsonTable(src.group));
+  x.use('/term', JsonTable(src.term));
+  x.use('/type', JsonTable(src.type));
+  x.use('/unit', JsonTable(src.unit));
   return x;
 };
