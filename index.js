9110093972de2cbdf775863d2520aa4a59ca13af
Subhajit Sahu <wolfram77@gmail.com>
2017-09-22 23:41:09 +0530

    :tv: where is the error

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const data = new Data(dbpool);
 const json = Json(data);
 const sql = new Sql(dbpool);
 server.listen(E.PORT||80);
-data.setup();
+data.setup().catch((err) => throw err);
 
 X.use(bodyParser.json());
 X.use(bodyParser.urlencoded({'extended': true}));
