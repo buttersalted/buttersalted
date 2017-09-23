f7df3431cf52eec2d0dc0f042bb81f17a5a86df4
Subhajit Sahu <wolfram77@gmail.com>
2017-09-23 23:08:08 +0530

    :tv: update sql

--- sql/index.js
+++ sql/index.js
@@ -1,8 +1,11 @@
 'use strict';
 const express = require('express');
+const body = (req) => Object.assign(req.body, req.query, req.params);
 
 module.exports = function(db) {
   const x = express();
-  x.get('/:val', (req, res, next) => db.query(req.params.val).then((ans) => res.send(ans.rows||[]), next));
+  x.get('/:val', (req, res, next) => db.query(body(req)).then((ans) =>
+    res.send(ans.rows||[]), next)
+  );
   return x;
 };
