b34edf41fecfccf2a5230dce70e6189ca5fa78af
Subhajit Sahu <wolfram77@gmail.com>
2017-09-24 12:34:02 +0530

    :tv: sql .val -> .value

--- sql/index.js
+++ sql/index.js
@@ -4,7 +4,7 @@ const body = (req) => Object.assign(req.body, req.query, req.params);
 
 module.exports = function(db) {
   const x = express();
-  x.get('/:val', (req, res, next) => db.query(body(req)).then((ans) =>
+  x.get('/:value', (req, res, next) => db.query(body(req).value).then((ans) =>
     res.send(ans.rows||[]), next)
   );
   return x;
