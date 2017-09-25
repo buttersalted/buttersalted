67aacf3793deff50cb684feda17d1e1940b0594f
Subhajit Sahu <wolfram77@gmail.com>
2017-09-25 17:01:35 +0530

    :tv: query value must be array

--- json/jsontable.js
+++ json/jsontable.js
@@ -6,7 +6,7 @@ module.exports = function JsonTable(src) {
   const x = express();
   x.get('/', (req, res, next) => src.select(body(req)).then((ans) => res.json(ans.rows||[]), next));
   x.post('/', (req, res, next) => src.insertOne(body(req)).then((ans) => res.json(1), next));
-  x.patch('/', (req, res, next) => src.call('refresh', body(req)).then((ans) => res.json(1), next));
+  x.patch('/', (req, res, next) => src.call('refresh', [body(req)]).then((ans) => res.json(1), next));
   x.get('/:id', (req, res, next) => src.selectOne(body(req)).then((ans) => res.json(ans.rows||[]), next));
   x.patch('/:id', (req, res, next) => src.upsertOne(body(req)).then((ans) => res.json(1), next));
   x.delete('/:id', (req, res, next) => src.deleteOne(body(req)).then((ans) => res.json(1), next));
