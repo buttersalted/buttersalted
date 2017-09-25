2dadc35332fa242938d34643cbbd28a7d773868b
Subhajit Sahu <wolfram77@gmail.com>
2017-09-25 15:36:55 +0530

    :tv: update query functions

--- data/utility.sql
+++ data/utility.sql
@@ -15,42 +15,14 @@ RETURNS TEXT[] AS $$
 $$ LANGUAGE SQL STRICT IMMUTABLE;
 
 
-CREATE OR REPLACE FUNCTION "query_wherelike" (JSON)
-RETURNS TEXT AS $$
-  -- 4. return where part
-  SELECT ' WHERE '||g
-  -- 3. aggregate all parts with and
-  FROM (SELECT string_agg(f, ' AND ') AS g
-  -- 2. create 'key LIKE value' parts
-  FROM (SELECT format('%I::TEXT LIKE %L', "key", "value") AS f
-  -- 1. get all keys and values
-  FROM json_each_text($1) t) u) v;
-$$ LANGUAGE SQL STRICT IMMUTABLE;
-
-
 CREATE OR REPLACE FUNCTION "query_format" (JSON, TEXT, TEXT, TEXT)
 RETURNS TEXT AS $$
-  -- 5. return set part
+  -- 5. return completed string
   SELECT $2||g
-  -- 4. aggregate all parts with comma
+  -- 4. aggregate all parts with separator
   FROM (SELECT string_agg(f, $4) AS g
-  -- 3. create 'key = value' parts
+  -- 3. create parts with given format
   FROM (SELECT format($3, "key",
-  -- 2. convert json double quotes to sql single
-    replace("value"::TEXT, E'\"'::TEXT, E'\''::TEXT)) AS f
-  -- 1. get all keys and values (as json)
-  FROM json_each($1) t) u) v;
-$$ LANGUAGE SQL STRICT IMMUTABLE;
-
-
-CREATE OR REPLACE FUNCTION "query_set" (JSON)
-RETURNS TEXT AS $$
-  -- 5. return set part
-  SELECT ' SET '||g
-  -- 4. aggregate all parts with comma
-  FROM (SELECT string_agg(f, ', ') AS g
-  -- 3. create 'key = value' parts
-  FROM (SELECT format('%I = %s', "key",
   -- 2. convert json double quotes to sql single
     replace("value"::TEXT, E'\"'::TEXT, E'\''::TEXT)) AS f
   -- 1. get all keys and values (as json)
@@ -60,17 +32,21 @@ $$ LANGUAGE SQL STRICT IMMUTABLE;
 
 CREATE OR REPLACE FUNCTION "query_selectlike" (TEXT, JSON)
 RETURNS TEXT AS $$
-  SELECT format('SELECT * FROM %I%s', $1, query_wherelike($2));
+  SELECT format('SELECT * FROM %I%s', $1,
+    query_format($2, ' WHERE ', '%I LIKE %s', ' AND '));
 $$ LANGUAGE SQL STRICT IMMUTABLE;
 
 
 CREATE OR REPLACE FUNCTION "query_deletelike" (TEXT, JSON)
 RETURNS TEXT AS $$
-  SELECT format('DELETE FROM %I%s', $1, query_wherelike($2));
+  SELECT format('DELETE FROM %I%s', $1,
+    query_format($2, ' WHERE ', '%I LIKE %s', ' AND '));
 $$ LANGUAGE SQL STRICT IMMUTABLE;
 
 
 CREATE OR REPLACE FUNCTION "query_updatelike" (TEXT, JSON, JSON)
 RETURNS TEXT AS $$
-  SELECT format('UPDATE %I%s%s', $1, query_set($2), query_wherelike($3));
+  SELECT format('UPDATE %I%s%s', $1,
+    query_format($2, ' SET ', '%I = %s', ', '),
+    query_format($3, ' WHERE ', '%I LIKE %s', ' AND '));
 $$ LANGUAGE SQL STRICT IMMUTABLE;
