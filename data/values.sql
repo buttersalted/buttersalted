c7d4bd2c69870e13cb2cbc20dab58b90e3decfea
Subhajit Sahu <wolfram77@gmail.com>
2017-09-26 22:35:34 +0530

    :tv: add manufacturer

--- data/values.sql
+++ data/values.sql
@@ -6,6 +6,7 @@ $$ LANGUAGE SQL;
 INSERT INTO "type" VALUES ('Id', 'INT NOT NULL')
 ON CONFLICT ("id") DO NOTHING;
 SELECT x('Name', 'TEXT NOT NULL');
+SELECT x('Manufacturer', 'TEXT');
 SELECT x('Food Group', 'TEXT');
 SELECT x('Carbohydrate Factor', 'REAL');
 SELECT x('Fat Factor', 'REAL');
