43679d0f923e6d4db42a435436a6fff603069abd
Subhajit Sahu <wolfram77@gmail.com>
2017-09-28 23:49:01 +0530

    :tv: javascript add onclick

--- html/type/index.js
+++ html/type/index.js
@@ -1,3 +1,9 @@
+const dosubmit = function() {
+  this.form[this.name].value = this.value;
+};
+for(var e in document.querySelectorAll('form [type=submit]'))
+  e.onclick = dosubmit;
+
 document.querySelector('form').onsubmit = function() {
   console.log(this.submitted);
   return false;
