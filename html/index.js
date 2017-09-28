2b9627511d4f47350751b833cea0da46162e114a
Subhajit Sahu <wolfram77@gmail.com>
2017-09-28 20:29:50 +0530

    :tv: render values

--- html/index.js
+++ html/index.js
@@ -1,3 +1,8 @@
 m.request({'method': 'GET', 'url': '/json/type'}).then((ans) => {
-  m.render(document.querySelector('thead'), ans.map((a) => m('th', a.id)));
+  const thead = document.querySelector('thead');
+  m.render(thead, m('tr', ans.map((a) => m('th', a.id))));
+});
+m.request({'method': 'GET', 'url': '/json/food'}).then((ans) => {
+  const tbody = document.querySelector('tbody');
+  m.render(tbody, ans.map((a) => m('tr', Object.keys(a).map((k) => m('td', a[k])))));
 });
