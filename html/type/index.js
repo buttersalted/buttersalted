19668e761078373964d5c00864e12c1453335fab
Subhajit Sahu <wolfram77@gmail.com>
2017-09-28 23:53:35 +0530

    :tv: use of

--- html/type/index.js
+++ html/type/index.js
@@ -1,7 +1,7 @@
 const dosubmit = function() {
   this.form[this.name].value = this.value;
 };
-for(var e in document.querySelectorAll('form [type=submit]'))
+for(var e of document.querySelectorAll('form [type=submit]'))
   e.onclick = dosubmit;
 
 document.querySelector('form').onsubmit = function() {
