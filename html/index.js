66b0d1687d0718b50d6985f355e9f08e56490585
Subhajit Sahu <wolfram77@gmail.com>
2017-09-29 19:44:42 +0530

    :tv: update index.js

--- html/index.js
+++ html/index.js
@@ -1,16 +1,26 @@
-var editor = ace.edit("sql-value");
-editor.setTheme("ace/theme/clouds");
-editor.getSession().setMode("ace/mode/pgsql");
-
-document.querySelector('#sql').onsubmit = function() {
-  const value = encodeURI(this.elements.value.value);
-  const thead = document.querySelector('thead');
-  const tbody = document.querySelector('tbody');
-  m.request({'method': 'GET', 'url': `/sql/${value}`}).then((ans) => {
-    if(!ans.length) return;
-    const cols = Object.keys(ans[0]);
-    m.render(thead, m('tr', cols.map((k) => m('th', k))));
+const renderTable = function(ans) {
+  const thead = document.querySelector('#sql-ans thead');
+  const tbody = document.querySelector('#sql-ans tbody');
+  if(!(ans instanceof Array)) m.render(thead, m('tr', m('th', ans)));
+  else if(!ans.length) m.render(thead, m('tr', m('th', 'no results.')));
+  else {
+    m.render(thead, m('tr', Object.keys(ans[0]).map((k) => m('th', k))));
     m.render(tbody, ans.map((r) => m('tr', Object.values(r).map((v) => m('td', v)))));
-  });
-  return false;
+  }
 };
+
+const onready = function() {
+  // 1. setup sql value editor
+  const sqlValue = ace.edit('sql-value');
+  sqlValue.setTheme('ace/theme/sqlserver');
+  sqlValue.getSession().setMode('ace/mode/pgsql');
+
+  // 2. setup sql get on click
+  const sqlGet = document.getElementById('sql-get')
+  sqlGet.onclick = function() {
+    const value = sqlValue.getValue();
+    m.request({'method': 'GET', 'url': `/sql/${value}`}).then(renderTable, renderTable);
+  };
+};
+
+onready();
