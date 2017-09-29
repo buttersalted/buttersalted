91c9fb31b5635ee0fbe7d4c893a3ca418ac1b86b
Subhajit Sahu <wolfram77@gmail.com>
2017-09-29 18:54:43 +0530

    :tv: use github theme

--- html/index.js
+++ html/index.js
@@ -1,5 +1,5 @@
 var editor = ace.edit("editor");
-editor.setTheme("ace/theme/monokai");
+editor.setTheme("ace/theme/github");
 editor.getSession().setMode("ace/mode/pgsql");
 
 document.querySelector('#sql').onsubmit = function() {
