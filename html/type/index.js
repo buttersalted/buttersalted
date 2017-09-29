9c7c40e2213ccfedc3689ca387208c0266c8742d
Subhajit Sahu <wolfram77@gmail.com>
2017-09-29 08:33:04 +0530

    :tv: use type

--- html/type/index.js
+++ html/type/index.js
@@ -4,7 +4,21 @@ const dosubmit = function() {
 for(var e of document.querySelectorAll('form [type=submit]'))
   e.onclick = dosubmit;
 
+const onrequest = function(ans) {
+  console.log(ans);
+  const thead = document.querySelector('thead');
+  const tbody = document.querySelector('tbody');
+  if(!(ans instanceof Array)) return m.render(thead, m('tr', m('th', ans)));
+  m.render(thead, m('tr', Object.keys(ans[0]).map((k) => m('th', k))));
+  m.render(tbody, ans.map((r) => m('tr', Object.values(r).map((v) => m('td', v)))));
+};
+
 document.querySelector('form').onsubmit = function() {
-  console.log(this.submitted);
+  const submit = this.submitted;
+  const data = {'id': this.elements.id.value, 'value': this.elements.value.value};
+  if(submit==='select') m.request({'method': 'GET', 'url': '/json/type', 'data': data}).then(onrequest);
+  else if(submit==='insert') m.request({'method': 'POST', 'url': '/json/type', 'data': data}).then(onrequest);
+  else if(submit==='upsert') m.request({'method': 'PATCH', 'url': `/json/type/${data.id}`, 'data': data}).then(onrequest);
+  else if(submit==='delete') m.request({'method': 'DELETE', 'url': `/json/type/${data.id}`}).then(onrequest);
   return false;
 };
