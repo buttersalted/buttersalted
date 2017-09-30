0c116f987aaf98b55a25e5739dcaf2dbc73678e9
Subhajit Sahu <wolfram77@gmail.com>
2017-10-01 00:37:58 +0530

    :tv: use request

--- html/unit/index.js
+++ html/unit/index.js
@@ -1,24 +1,22 @@
 const render = function(ans) {
   const tbody = document.querySelector('#unitans tbody');
-  if(ans instanceof Array && ans.length) {
-    m.render(thead, m('tr', Object.keys(ans[0]).map((k) => m('th', k))));
-    m.render(tbody, ans.map((r) => m('tr', Object.values(r).map((v) => m('td', v)))));
-  }
-  else {
-    if(ans instanceof Error) m.render(thead, m('tr', m('th', ans.message)));
-    else m.render(thead, m('tr', m('th', 'no results.')));
-    m.render(tbody, null);
-  }
+  m.render(tbody, ans.map((r) => m('tr', Object.values(r).map((v) => m('td', v)))));
+};
+
+const errornotify = function(err) {
+  iziToast.error({'title': 'Serach Error', 'message': err.message});
 };
 
 const onready = function() {
   // 1. setup form usage
   const unit = document.getElementById('unit')
   unit.onsubmit = function() {
-    const id = this.elements.id.value;
-    const value = this.elements.value.value;
+    var id = this.elements.id.value;
+    var value = this.elements.value.value;
     const data = {'id': id, 'value': value};
-    m.request({'method': 'GET', 'url': '/json/unit', 'data': data}).then(render, render);
+    m.request({'method': 'GET', 'url': '/json/unit', 'data': data}).then(
+      render, errornotify
+    );
   };
 };
 
