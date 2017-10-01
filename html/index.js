5d29e2d4d1dd30ece519c94c835c06c03b10967d
Subhajit Sahu <wolfram77@gmail.com>
2017-10-01 13:51:47 +0530

    :tv: setup based on url

--- html/index.js
+++ html/index.js
@@ -1,3 +1,32 @@
+// global variables
+const Editor = ace.edit('sql-value');
+const Header = document.querySelector('header');
+const Navs = document.querySelectorAll('nav li a');
+const Divs = document.querySelectorAll('main > div');
+
+const urlSetup = function(url) {
+  // 1. get url path (form a full url)
+  url = url.replace(location.origin, '');
+  url = url.replace('/#!/', '');
+  url = url.startsWith('/')? url.substring(1) : url;
+  // 2. get path prefix
+  const pre = url.split('/')[0];
+  // 3. update navigation menu
+  for(var i=0, I=Navs.length; i<I; i++) {
+    if(Navs[i].id===pre) Navs[i].setAttribute('active', '');
+    else Navs[i].removeAttribute('active');
+  }
+  // 4, update main view
+  for(var i=0, I=Divs.length; i<I; i++) {
+    if(Divs[i].id===pre) Divs[i].hidden = false;
+    else Divs[i].hidden = true;
+  }
+  // 5.set the location (and live happily ever after)
+  location.href = location.origin+(url? '/#!/'+url : '');
+};
+
+
+
 const renderTable = function(ans) {
   const thead = document.querySelector('#sql-ans thead');
   const tbody = document.querySelector('#sql-ans tbody');
@@ -12,18 +41,29 @@ const renderTable = function(ans) {
   }
 };
 
-const onready = function() {
-  // 1. setup sql value editor
-  const sqlValue = ace.edit('sql-value');
-  sqlValue.setTheme('ace/theme/sqlserver');
-  sqlValue.getSession().setMode('ace/mode/pgsql');
+const relocate = function(url) {
+  const pre = url.substr
+  if(!url) Header.hidden = false;
+  for(var i=0, I=Navs.length; i<I; i++)
+    if(Navs[i].textContent.toLowerCase()===)
+  location.href = location.origin+(url? '/#!/'+url : '');
+};
 
+const setup = function() {
+  // 1. setup ace editor
+  Editor.setTheme('ace/theme/sqlserver');
+  Editor.getSession().setMode('ace/mode/pgsql');
+
+  /*
   // 2. setup sql get on click
   const sqlGet = document.getElementById('sql-get')
   sqlGet.onclick = function() {
     const value = sqlValue.getValue();
     m.request({'method': 'GET', 'url': `/sql/${value}`}).then(renderTable, renderTable);
   };
+  */
+  // 2. setup url
+  urlSetup(location.href);
 };
 
-onready();
+setup();
