39d0d8e2e762636818ec0d31bb80cc3c6f73421e
Subhajit Sahu <wolfram77@gmail.com>
2017-10-04 04:23:58 +0530

    :tv: respond to get on /sql too

--- sql/index.js
+++ sql/index.js
@@ -4,13 +4,15 @@ const body = (req) => Object.assign(req.body, req.query, req.params);
 
 module.exports = function(db) {
   const x = express();
-  x.get('/:value', (req, res, next) => {
+  const fn = (req, res, next) => {
     const qry = body(req).value||'';
     if(qry.includes(';') ||
       !qry.toUpperCase().startsWith('SELECT ') ||
       qry.toUpperCase().includes('INTO'))
       throw new Error('bad query');
     db.query(qry).then((ans) => res.send(ans.rows||[]), next)
-  });
+  };
+  x.get('/', fn);
+  x.get('/:value', fn);
   return x;
 };
