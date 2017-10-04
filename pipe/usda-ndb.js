0326a22a71e1d19e95d7656ffeaeeca9ea0b550f
Subhajit Sahu <wolfram77@gmail.com>
2017-10-04 16:57:50 +0530

    :tv: handle only 1 id in pipe

--- pipe/usda-ndb.js
+++ pipe/usda-ndb.js
@@ -3,21 +3,6 @@ const express = require('express');
 const usdaNdb = require('usda-ndb');
 const body = (req) => Object.assign(req.body, req.query, req.params);
 
-const each = function(a, fn) {
-  // 1. get start, stop, step, inc
-  const start = parseInt(a.start)||0, stop = parseInt(a.stop)||start+1;
-  const step = parseInt(a.step)||1, inc = Math.sign(step);
-  const fetch = (id) => pro.then(() => usdaNdb(id)).then((ans) => fn(id, ans));
-  // 2. get object for every id, and call the callback fn
-  for(var i=start, pro=Promise.resolve(); i!==stop;) {
-    for(var I=Math.min(stop, i+step), p=[]; i!==I; i+=inc)
-      p.push(fetch(i));
-    pro = Promise.all(p);
-  }
-  // 3. return all objects got promise
-  return pro;
-};
-
 const convert = function(a) {
   // 1. convert object to row format
   const k = Object.keys(a)[0], v = a[k];
@@ -29,26 +14,16 @@ const convert = function(a) {
 module.exports = function(dst) {
   const x = express();
   // 1. setup get from usda-ndb
-  x.get('/', (req, res, next) => {
-    const z = {};
-    each(body(req), (id, ans) => z[id] = convert(ans)).then(() => {
-      res.json(z);
-    }, next);
-  });
+  const get = (req, res, next) => usdaNdb(body(req).id).then((ans) => {
+    res.json(convert(ans));
+  }, next);
+  x.get('/', get);
+  x.get('/:id', get);
   // 2. setup insert using usda-ndb
-  x.post('/', (req, res, next) => {
-    var z = {}, p = [], e = 0;
-    // 1. get all objects and insert as rows
-    each(body(req), (id, ans) => {
-      const row = convert(ans);
-      p.push(dst.insertOne(row).then(
-        (ans) => z[id] = ans.rowCount,
-        (err) => {e = 1; return z[id] = err.message;}
-      ));
-    // 2. return status of each insert
-    }).then(() => Promise.all(p)).then(() => {
-      (e? res.status(400) : res).json(z);
-    }, next);
-  });
+  const post = (req, res, next) => usdaNdb(body(req).id).then((ans) => {
+    dst.insertOne(convert(ans)).then((ans) => res.json(ans), next);
+  }, next);
+  x.post('/', post);
+  x.post('/:id', post);
   return x;
 };
