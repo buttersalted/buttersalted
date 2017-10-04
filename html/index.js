3834e618678f361dc14a5802e3801d3565ace95f
Subhajit Sahu <wolfram77@gmail.com>
2017-10-04 21:22:28 +0530

    :tv: error toasts

--- html/index.js
+++ html/index.js
@@ -183,7 +183,7 @@ var formPipe = function() {
     return ajaxReq('GET', url+i).then(function(ans) {
       z.push(ans);
       ansRender(z);
-    });
+    }, ansError);
   }, parseInt(data.start), parseInt(data.stop));
   // 4. if submit is post, render status (yay async again)
   else if(sbt==='post') loopAsync(function(i) {
