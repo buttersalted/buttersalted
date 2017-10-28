c15202f8d517105253d68a9cf68c9cc67f1cbcbf
Subhajit Sahu <wolfram77@gmail.com>
2017-10-29 00:59:00 +0530

    :telephone_receiver: log pipe setup

--- pipe/index.js
+++ pipe/index.js
@@ -5,5 +5,6 @@ const usdaNdb = require('./usda-ndb');
 module.exports = function(dst) {
   const x = express();
   x.use('/usda-ndb', usdaNdb(dst.food));
+  console.log('pipe: setup');
   return x;
 };
