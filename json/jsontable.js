da7072e53f64e84dcc17b31e7a01b263b2c6abe5
Subhajit Sahu <wolfram77@gmail.com>
2017-11-02 00:54:28 +0530

    :telephone_receiver: disable patch on root url

--- json/jsontable.js
+++ json/jsontable.js
@@ -6,7 +6,6 @@ module.exports = function JsonTable(src) {
   const x = express();
   x.get('/', (req, res, next) => src.select(body(req)).then((ans) => res.json(ans.rows||[]), next));
   x.post('/', (req, res, next) => src.insertOne(body(req)).then((ans) => res.json(1), next));
-  x.patch('/', (req, res, next) => src.updateOne(body(req), body(req)).then((ans) => res.json(1), next));
   x.get('/:id', (req, res, next) => src.selectOne(req.params).then((ans) => res.json(ans.rows||[]), next));
   x.patch('/:id', (req, res, next) => src.updateOne(req.params, body(req)).then((ans) => res.json(1), next));
   x.delete('/:id', (req, res, next) => src.deleteOne(req.params).then((ans) => res.json(1), next));
