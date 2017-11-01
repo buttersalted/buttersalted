d8c5d39cc2114a51a203d3637857140cfe033f36
Subhajit Sahu <wolfram77@gmail.com>
2017-11-02 00:57:45 +0530

    :telephone_receiver: limit to max 32 results

--- json/jsontable.js
+++ json/jsontable.js
@@ -4,7 +4,7 @@ const body = (req) => Object.assign(req.body, req.query);
 
 module.exports = function JsonTable(src) {
   const x = express();
-  x.get('/', (req, res, next) => src.select(body(req)).then((ans) => res.json(ans.rows||[]), next));
+  x.get('/', (req, res, next) => src.select(body(req), 32).then((ans) => res.json(ans.rows||[]), next));
   x.post('/', (req, res, next) => src.insertOne(body(req)).then((ans) => res.json(1), next));
   x.get('/:id', (req, res, next) => src.selectOne(req.params).then((ans) => res.json(ans.rows||[]), next));
   x.patch('/:id', (req, res, next) => src.updateOne(req.params, body(req)).then((ans) => res.json(1), next));
