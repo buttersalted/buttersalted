7b4ad6931c9eb20b1dfed3b04b951c18e29db5f4
Subhajit Sahu <wolfram77@gmail.com>
2017-11-01 09:14:03 +0530

    :telephone_receiver: add mass units (short)

--- data/unit.sql
+++ data/unit.sql
@@ -1,4 +1,4 @@
--- 1. create table to store unit conversion factors
+-- I. create table to store unit conversion factors
 CREATE TABLE IF NOT EXISTS "unit" (
   "id"     TEXT NOT NULL,
   "factor" REAL NOT NULL DEFAULT 1,
@@ -26,12 +26,14 @@ $$ LANGUAGE SQL STABLE;
 
 CREATE OR REPLACE FUNCTION "unit_selectone" (JSONB)
 RETURNS SETOF "unit" AS $$
+  -- 1. select a row
   SELECT * FROM "unit" WHERE "id"=$1->>'id';
 $$ LANGUAGE SQL;
 
 
 CREATE OR REPLACE FUNCTION "unit_insertone" (JSONB)
 RETURNS VOID AS $$
+  -- 1. insert a row
   INSERT INTO "unit" SELECT * FROM
   jsonb_populate_record(NULL::"unit", table_default('unit')||$1);
 $$ LANGUAGE SQL;
@@ -39,15 +41,31 @@ $$ LANGUAGE SQL;
 
 CREATE OR REPLACE FUNCTION "unit_deleteone" (JSONB)
 RETURNS VOID AS $$
+  -- 1. delete a row
   DELETE FROM "unit" WHERE "id"=$1->>'id';
 $$ LANGUAGE SQL;
 
 
 CREATE OR REPLACE FUNCTION "unit_updateone" (JSONB, JSONB)
 RETURNS VOID AS $$
+  -- 1. update a row
   UPDATE "unit" SET "id"=coalesce($2->>'id', r."id"),
   "factor"=coalesce(($2->>'factor')::REAL, r."factor"),
   "offset"=coalesce(($2->>'offset')::REAL, r."offset")
   FROM (SELECT * FROM "unit" WHERE "id"=$1->>'id') r
   WHERE "id"=$1->'id';
 $$ LANGUAGE SQL;
+
+
+-- II. insert default data
+CREATE OR REPLACE FUNCTION x(TEXT, REAL, REAL)
+RETURNS VOID AS $$
+  -- 1. insert a row, if not exists
+  INSERT INTO "unit" VALUES ($1, $2, $3) ON CONFLICT ("id") DO NOTHING;
+$$ LANGUAGE SQL;
+SELECT x('', 1, 0);
+SELECT x('ng', 1e-12, 0);
+SELECT x('μg', 1e-9, 0);
+SELECT x('mg', 1e-6, 0);
+SELECT x('g', 1e-3, 0);
+SELECT x('kg', 1, 0);
