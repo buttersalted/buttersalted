8f7c8c097888a762f56441d214490b748a0504a0
Subhajit Sahu <wolfram77@gmail.com>
2017-11-01 08:39:01 +0530

    :telephone_receiver: comments in unit (comments are really important, BELIEVE, ME)

--- data/unit.sql
+++ data/unit.sql
@@ -10,6 +10,7 @@ CREATE TABLE IF NOT EXISTS "unit" (
 
 CREATE OR REPLACE FUNCTION "unit_get" (TEXT)
 RETURNS "unit" AS $$
+  -- 1. get exact or lowercase matching unit
   SELECT * FROM "unit" WHERE "id"=$1 UNION ALL
   SELECT * FROM "unit" WHERE "id"=lower($1) LIMIT 1;
 $$ LANGUAGE SQL STABLE;
@@ -17,6 +18,7 @@ $$ LANGUAGE SQL STABLE;
 
 CREATE OR REPLACE FUNCTION "unit_convert" (REAL, TEXT, TEXT)
 RETURNS REAL AS $$
+  -- 1. convert number from one unit to another
   SELECT ($1*f."factor"+f."offset"-t."offset")/t."factor"
   FROM unit_get($2) f, unit_get($3) t;
 $$ LANGUAGE SQL STABLE;
