29243b1491313a787bb465c8c6aaa99de2a55bdb
Subhajit Sahu <wolfram77@gmail.com>
2017-11-06 06:57:51 +0530

    :telephone_receiver: update nutritionvalue-searchall pipe to new format

--- pipe/nutritionvalue-searchall.js
+++ pipe/nutritionvalue-searchall.js
@@ -3,25 +3,24 @@ const express = require('express');
 const searchall = require('nutritionvalue-searchall');
 const body = (req) => Object.assign(req.body, req.query);
 
-const convert = function(i, a) {
+const convert = function(a) {
   // 1. get object key (id, name)
-  const k = Object.keys(a)[0];
-  if(!k) return a;
+  const i = Object.keys(a)[0];
+  if(!i) return a;
   // 2. convert object to row format
-  const Id = i, Name = k;
-  return Object.assign({Id, Name}, a[k]);
+  return Object.assign(a[i], {'Id': i});
 };
 
 module.exports = function(dst) {
   const x = express();
   // 1. setup get from nutritionvalue-searchall
   x.get('/:id', (req, res, next) => searchall(req.params.id).then((ans) => {
-    res.json(convert(req.params.id, ans));
+    res.json(convert(ans));
   }, next));
   // 2. setup insert using nutritionvalue-searchall
   x.post('/', (req, res, next) => searchall(body(req).id).then((ans) => {
     if(!Object.keys(ans).length) return next(new Error('not available'));
-    dst.insertOne(convert(body(req).id, ans)).then((ans) => res.json(ans), next);
+    dst.insertOne(convert(ans)).then((ans) => res.json(ans), next);
   }, next));
   return x;
 };
