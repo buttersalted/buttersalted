37b4a8cac19bf7c860966f853dd37b57c9afd1d7
Subhajit Sahu <wolfram77@gmail.com>
2017-11-06 06:55:16 +0530

    :telephone_receiver: update usda-ndb pipe to new format

--- pipe/usda-ndb.js
+++ pipe/usda-ndb.js
@@ -3,26 +3,27 @@ const express = require('express');
 const ndb = require('usda-ndb');
 const body = (req) => Object.assign(req.body, req.query);
 
-const convert = function(i, a) {
+const convert = function(a) {
   // 1. get object key (id, name)
-  const k = Object.keys(a)[0];
-  if(!k) return a;
+  const v = Object.values(a)[0];
+  if(!v) return a;
   // 2. convert object to row format
-  const Id = parseInt(k.substring(0, k.indexOf(',')));
-  const Name = k.substring(k.indexOf(',')+1).trim();
-  return Object.assign({Id, Name}, a[k]);
+  const n = v.Name;
+  const Id = parseInt(n.substring(0, n.indexOf(',')));
+  const Name = n.substring(n.indexOf(',')+1).trim();
+  return Object.assign(v, {Id, Name});
 };
 
 module.exports = function(dst) {
   const x = express();
   // 1. setup get from usda-ndb
   x.get('/:id', (req, res, next) => ndb(req.params.id).then((ans) => {
-    res.json(convert(req.params.id, ans));
+    res.json(convert(ans));
   }, next));
   // 2. setup insert using usda-ndb
   x.post('/', (req, res, next) => ndb(body(req).id).then((ans) => {
     if(!Object.keys(ans).length) return next(new Error('not available'));
-    dst.insertOne(convert(body(req).id, ans)).then((ans) => res.json(ans), next);
+    dst.insertOne(convert(ans)).then((ans) => res.json(ans), next);
   }, next));
   return x;
 };
