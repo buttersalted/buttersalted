2174238ba15a21d0358edd19f5094a3cf86c1908
Subhajit Sahu <wolfram77@gmail.com>
2017-11-07 11:11:08 +0530

    :telephone_receiver: use datatables, keep data in current sort order

--- html/index.js
+++ html/index.js
@@ -86,7 +86,8 @@ function ansRender(ans) {
   m.render(Tbody, ans.length? zv : null);
   Tdiv.height = Thead.height;
   // 5. show toast message (if empty)
-  if(!ca.length) toast('warning', 'Empty Query', 'No values returned');
+  if(ca.length) $(Thead.parentElement).DataTable({order: []});
+  else toast('warning', 'Empty Query', 'No values returned');
 };
 
 function ansError(err) {
